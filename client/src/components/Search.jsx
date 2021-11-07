d589904339652498ac70c2df458fc1906392791f
Mannan Baidi <68608356+baidi-mannan@users.noreply.github.com>
2021-11-07 13:03:51 +0530

    search  page updated

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,15 +1,37 @@
-import React  from 'react';
+import React,{useState, useEffect}  from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Typography, Button } from '@material-ui/core'
+import { Button } from '@material-ui/core'
 import {AiOutlineSearch} from 'react-icons/ai'
-
+import List from '../images/list.json'
+import Rent from './Cards/Rent.jsx'
 const Search=()=>{
+
+    const [search, setSearch]= useState("")
+    const [searchList, setSearchList]= useState(List)
+    const searchUpdate=(e)=>{
+        e.preventDefault();
+        setSearch(e.target.value)
+    }
+   const updateList=()=>{
+
+    console.log(search)
+   }
+    useEffect(()=>
+    {
+       
+        updateList();
+        
+
+    },[search])
+
     const useStyles = makeStyles({
         root: {
             marginTop:"60px",
     
             display: "flex",
             justifyContent:"center",
+            flexDirection:"column",
+            "alignItems":"center",
             backgroundColor:"#eaeaea"
 
         },
@@ -33,14 +55,26 @@ const Search=()=>{
  
        
        })
+
        const classes = useStyles();
     return (
 
         <>
         <div className={classes.root}>
         <div className={classes.searchBox}>
-        <input placeholder="Search by City" className={classes.input} />
+        <input placeholder="Search by City" value={search} onChange={searchUpdate} className={classes.input} />
         <Button type="submit" variant="contained" color="primary" style={{height:"52px",fontSize:"18px",width:"200px",textTransform: 'capitalize',display:"flex",justifyContent:"space-around",padding:" 0 50px",borderRadius:"0"}}><AiOutlineSearch/> Search</Button>
+        </div>
+
+        <div>
+        {searchList.map((ele, i) => {     
+                  return <Rent/>    
+          
+        })}
+
+
+
+
         </div>
         </div>
         </>
