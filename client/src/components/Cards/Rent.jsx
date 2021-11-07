43ab558e9aef6da12011999002cc2e70b88d76d1
Mannan Baidi <68608356+baidi-mannan@users.noreply.github.com>
2021-11-07 13:09:27 +0530

    Add files via upload

---
+++ client/src/components/Cards/Rent.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Button } from '@material-ui/core'
+
+
+const Rent=()=>
+{
+    
+    return (
+        <>
+        hyyy
+
+        </>
+
+    )
+}
+
+export default Rent
